ea058b5073b0e509ab47f6296ff8b98925645c6f
Samuel Gutierrez Casafus <Samuelgutierrezc15@gmail.com>
2025-12-05 22:26:39 -0500

    test: adaptar test a la API nativa de node --test

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,8 +1,7 @@
-const assert = require("assert/strict");
+const test = require("node:test");
+const assert = require("node:assert/strict");
 const { sumar } = require("../src/app.js");
 
-describe("sumar", () => {
-  it("suma correctamente 2 + 3", () => {
-    assert.equal(sumar(2, 3), 5);
-  });
+test("sumar 2 + 3 = 5", () => {
+  assert.equal(sumar(2, 3), 5);
 });
